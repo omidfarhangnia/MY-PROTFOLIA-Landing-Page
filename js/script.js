7f6bd7fd59169fc9660dc1570ec59dfbeff27edb
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2023-02-02 22:45:39 +0330

    adding the style of sections

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 gsap.registerPlugin(ScrollTrigger)
 
+// scripts for changing the bg of header in scrolling
 gsap.to(".header", {
     background: "rgb(70, 70, 70)",
     scrollTrigger: {
@@ -10,7 +11,7 @@ gsap.to(".header", {
     }
 })
 
-
+// scripts for changing the color of text and their position in scrolling
 gsap.to(".data__container, .arrow__container", {
     y: "80vh",
     color: "rgb(51, 51, 51)",
